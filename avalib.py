65fda1bf055b8188e80e188fce5050526a0bb32a
kjwitt <witt.87@osu.edu>
2014-07-29 17:17:47 -0400

    Added clock  to UI

--- avalib.py
+++ avalib.py
@@ -6,8 +6,8 @@ import urllib2
 import time
 import datetime
 
-slurple = '#682860'
-kraagle = '#FFFFF0'
+indigo = '#1b0432'
+cream = '#FFFFF0'
 
 def num2binarray(dec_no,size):
    bool_array = []
@@ -99,9 +99,9 @@ class Calendar:
       self.date_mo = 'MM'
       self.date_yr = 'YYYY'
       self.date_str = 'DD.MM.YYYY'
-      self.time_min = 'mm'
+      self.time_mn = 'mm'
       self.time_hr = 'hr'
-      self.time_min_bin = []
+      self.time_mn_bin = []
       self.time_hr_bin = []
       self.dow = []
       self.update()
@@ -117,8 +117,8 @@ class Calendar:
       self.date_str =  self.date_dy+'.'+self.date_mo+'.'+self.date_yr
       self.time_hr = datetime.datetime.now().time().hour
       self.time_hr_bin = num2binarray(datetime.datetime.now().time().hour,5)
-      self.time_min = datetime.datetime.now().time().minute
-      self.time_min_bin = num2binarray(datetime.datetime.now().time().minute,6)
+      self.time_mn = datetime.datetime.now().time().minute
+      self.time_mn_bin = num2binarray(datetime.datetime.now().time().minute,6)
 
 class UI(Frame):
    def __init__(self, parent):
@@ -129,11 +129,73 @@ class UI(Frame):
       self.parent.focus_set()
       self.ava = AVA()
       self.pack(fill=BOTH, expand=1)
-      self.configure(background = slurple)
+      self.configure(background = indigo)
       self.init_clock()
+      self.grid_columnconfigure(0,minsize=20)
+      self.grid_rowconfigure(0,minsize=20)
+      self.grid_rowconfigure(8,minsize=40)
    def init_clock(self):
-      lbl_date = Label(self, text=self.ava.cal.date_str, fg=kraagle, bg=slurple,font='Consolas')
-      lbl_date.grid(sticky=W, pady=4, padx=5,column=0,row=5)
+      self.lbl_date = Label(self, text=self.ava.cal.date_str, fg=cream, bg=indigo,font=('Consolas',19))
+      self.lbl_date.grid(column=1,row=6,columnspan=3)
+
+      self.lbl_hr = Label(self,text=self.ava.cal.time_hr, fg=cream, bg=indigo,font=('Consolas',19))
+      self.lbl_hr.grid(column=2,row=8)
+
+      self.lbl_mn = Label(self,text=self.ava.cal.time_mn, fg=cream, bg=indigo,font=('Consolas',19))
+      self.lbl_mn.grid(column=3,row=8)
+
+      self.cnv_dow = Canvas(self,width=40,height=280)
+      self.cnv_dow.grid(sticky=S,column=1,row=7,padx=4,rowspan=2)
+      self.dow_rect_bin = []
+      self.dow_rect_bin.append(self.cnv_dow.create_rectangle(3,2,40,42,fill=indigo,outline=indigo))
+      self.dow_rect_bin.append(self.cnv_dow.create_rectangle(3,42,40,82,fill=indigo,outline=indigo))
+      self.dow_rect_bin.append(self.cnv_dow.create_rectangle(3,82,40,122,fill=indigo,outline=indigo))
+      self.dow_rect_bin.append(self.cnv_dow.create_rectangle(3,122,40,162,fill=indigo,outline=indigo))
+      self.dow_rect_bin.append(self.cnv_dow.create_rectangle(3,162,40,202,fill=indigo,outline=indigo))
+      self.dow_rect_bin.append(self.cnv_dow.create_rectangle(3,202,40,242,fill=indigo,outline=indigo))
+      self.dow_rect_bin.append(self.cnv_dow.create_rectangle(3,242,40,282,fill=indigo,outline=indigo))
+      self.set_bin(self.cnv_dow,self.ava.cal.dow,self.dow_rect_bin)
+      self.cnv_dow.create_text(21.5,22,fill=indigo,text='SUN',font=('Consolas',16))
+      self.cnv_dow.create_text(21.5,62,fill=indigo,text='SAT',font=('Consolas',16))
+      self.cnv_dow.create_text(21.5,102,fill=indigo,text='FRI',font=('Consolas',16))
+      self.cnv_dow.create_text(21.5,142,fill=indigo,text='THU',font=('Consolas',16))
+      self.cnv_dow.create_text(21.5,182,fill=indigo,text='WED',font=('Consolas',16))
+      self.cnv_dow.create_text(21.5,222,fill=indigo,text='TUE',font=('Consolas',16))
+      self.cnv_dow.create_text(21.5,262,fill=indigo,text='MON',font=('Consolas',16))
+
+
+      self.cnv_hr = Canvas(self,width=40,height=200)
+      self.cnv_hr.grid(sticky=S,column=2,row=7,padx=4)
+      self.hr_bin = []
+      self.hr_bin.append(self.cnv_hr.create_rectangle(3,2,40,42,fill=indigo,outline=indigo))
+      self.hr_bin.append(self.cnv_hr.create_rectangle(3,42,40,82,fill=indigo,outline=indigo))
+      self.hr_bin.append(self.cnv_hr.create_rectangle(3,82,40,122,fill=indigo,outline=indigo))
+      self.hr_bin.append(self.cnv_hr.create_rectangle(3,122,40,162,fill=indigo,outline=indigo))
+      self.hr_bin.append(self.cnv_hr.create_rectangle(3,162,40,202,fill=indigo,outline=indigo))
+      self.set_bin(self.cnv_hr,self.ava.cal.time_hr_bin,self.hr_bin)
+
+      self.cnv_mn = Canvas(self,width=40,height=240)
+      self.cnv_mn.grid(sticky=S,column=3,row=7,padx=4)
+      self.mn_bin = []
+      self.mn_bin.append(self.cnv_mn.create_rectangle(3,2,40,42,fill=indigo,outline=indigo))
+      self.mn_bin.append(self.cnv_mn.create_rectangle(3,42,40,82,fill=indigo,outline=indigo))
+      self.mn_bin.append(self.cnv_mn.create_rectangle(3,82,40,122,fill=indigo,outline=indigo))
+      self.mn_bin.append(self.cnv_mn.create_rectangle(3,122,40,162,fill=indigo,outline=indigo))
+      self.mn_bin.append(self.cnv_mn.create_rectangle(3,162,40,202,fill=indigo,outline=indigo))
+      self.mn_bin.append(self.cnv_mn.create_rectangle(3,202,40,242,fill=indigo,outline=indigo))
+      self.set_bin(self.cnv_mn,self.ava.cal.time_mn_bin,self.mn_bin)
+
+
+   def set_bin(self,cnv,ctrl_array,ui_array):
+      print ctrl_array
+      print ui_array
+      for ctrl,ui in zip(ctrl_array,reversed(ui_array)):
+         if ctrl==True:
+            cnv.itemconfig(ui,fill=cream,outline=cream)
+         else:
+            cnv.itemconfig(ui,fill=indigo,outline=indigo)
+
+      
 
    def exit(self,event):
       # query = repr(event.char)
