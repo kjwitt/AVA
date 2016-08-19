0a7dd1bbc6e4405f6043bef45d2da768659e2b8e
kjwitt <kjwitt87@gmail.com>
2016-08-19 15:28:29 -0700

    text to speech with ivona works on raspberry pi
    no longer cross platform with windows :-1:

--- backend/backend/Program.cs
+++ backend/backend/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Media;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,13 +11,25 @@ namespace Backend
 {
     class Program
     {
+
+        static SoundPlayer player = new SoundPlayer();
+
         static void Main(string[] args)
         {
             //Messenger lol = new Messenger();
             //lol.WriteMessage("Test", "Brielle, you might get some messages because I'm testing my new framework. Sorry in advance.");
+            TTS lol = new Backend.TTS();
 
-            PIR PIRSensor = new PIR();
-            
+            lol.Speak("Ok here goes nothing...");
         }
+
+        /*static public void PlaySound(Stream sound)
+        {
+            
+            sound.Position = 0;     // Manually rewind stream 
+            player.Stream = null;    // Then we have to set stream to null 
+            player.Stream = sound;  // And set it again, to force it to be loaded again... 
+            player.Play();          // Yes! We can play the sound! 
+        }*/
     }
 }
